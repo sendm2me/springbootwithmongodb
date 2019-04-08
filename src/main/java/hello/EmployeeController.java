0b66722fb90c78493589bca89d40ce04fef9c0d9
sendm2me <sendm2me@gmail.com>
2019-04-07 22:30:30 -0400

    Update EmployeeController.java

--- src/main/java/hello/EmployeeController.java
+++ src/main/java/hello/EmployeeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 
 @RestController
-public class GreetingController {
+public class EmployeeController {
 
 	@Autowired
 	private EmployeeRepository repository;
