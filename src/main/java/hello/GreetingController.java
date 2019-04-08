cf7dde12a3774573c29a334b63dc8bcaaaf36f46
sendm2me <sendm2me@gmail.com>
2019-04-07 20:19:21 -0400

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -4,6 +4,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 public class GreetingController {
