96cd653676a2dba9ccd902a07d2ad6195b5c8af8
sendm2me <sendm2me@gmail.com>
2019-04-07 20:47:05 -0400

    Update EmployeeRepository.java

--- src/main/java/hello/EmployeeRepository.java
+++ src/main/java/hello/EmployeeRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 public interface EmployeeRepository extends MongoRepository<Employee, String> {
 
     public Employee findByFirstName(String firstName);
-    public List<Emplopyee> findByLastName(String lastName);
+    public List<Employee> findByLastName(String lastName);
 
 }
